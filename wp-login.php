cb3f84cf18ea259592911aaa1079d3941760e29a
ruhail99 <royalruhailali@gmail.com>
2022-09-29 16:24:16 +0530

    up

--- wp-login.php
+++ wp-login.php
@@ -5,17 +5,17 @@
 	<title>Log In &lsaquo; Digital Baikunthpur &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://digitalbaikunthpur.online/wp-includes/css/dashicons.min.css?ver=6.0.2" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://digitalbaikunthpur.online/wp-includes/css/buttons.min.css?ver=6.0.2" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://digitalbaikunthpur.online/wp-admin/css/forms.min.css?ver=6.0.2" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://digitalbaikunthpur.online/wp-admin/css/l10n.min.css?ver=6.0.2" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://digitalbaikunthpur.online/wp-admin/css/login.min.css?ver=6.0.2" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=6.0.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=6.0.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=6.0.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=6.0.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=6.0.2" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="https://digitalbaikunthpur.online/wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png" sizes="32x32">
-<link rel="icon" href="https://digitalbaikunthpur.online/wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png" sizes="192x192">
-<link rel="apple-touch-icon" href="https://digitalbaikunthpur.online/wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png">
-<meta name="msapplication-TileImage" content="https://digitalbaikunthpur.online/wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png">
+	<link rel="icon" href="./wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png" sizes="32x32">
+<link rel="icon" href="./wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png" sizes="192x192">
+<link rel="apple-touch-icon" href="./wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png">
+<meta name="msapplication-TileImage" content="./wp-content/uploads/2022/09/DigitalBaikunthpur-icon-1.png">
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
@@ -46,40 +46,40 @@
 		</form>
 
 					<p id="nav">
-								<a href="https://digitalbaikunthpur.online/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="./?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="https://digitalbaikunthpur.online/">&larr; Go to Digital Baikunthpur</a>		</p>
+			<a href="./index.html">&larr; Go to Digital Baikunthpur</a>		</p>
 			</div>
-			<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+			<script type="text/javascript" src="./wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="./wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">/*  */
-var _zxcvbnSettings = {"src":"https:\/\/digitalbaikunthpur.online\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":".\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */</script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
+<script type="text/javascript" src="./wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/hooks.min.js?ver=c6d64f2cb8f5c6bb49caca37f8828ce3" id="wp-hooks-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/i18n.min.js?ver=ebee46757c6a411e38fd079a7ac71d94" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
 <script type="text/javascript" id="password-strength-meter-js-extra">/*  */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
 /*  */</script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-admin/js/password-strength-meter.min.js?ver=6.0.2" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
+<script type="text/javascript" src="./wp-admin/js/password-strength-meter.min.js?ver=6.0.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="./wp-includes/js/underscore.min.js?ver=1.13.3" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">/*  */
 var _wpUtilSettings = {"ajax":{"url":"\/digitalbaikunthpur\/wp-admin\/admin-ajax.php"}};
 /*  */</script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-includes/js/wp-util.min.js?ver=6.0.2" id="wp-util-js"></script>
+<script type="text/javascript" src="./wp-includes/js/wp-util.min.js?ver=6.0.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">/*  */
 var userProfileL10n = {"user_id":"0","nonce":"0c1c2f29b7"};
 /*  */</script>
-<script type="text/javascript" src="https://digitalbaikunthpur.online/wp-admin/js/user-profile.min.js?ver=6.0.2" id="user-profile-js"></script>
+<script type="text/javascript" src="./wp-admin/js/user-profile.min.js?ver=6.0.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
